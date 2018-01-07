1df26644a9afa0f6310c3e75469b6ca0ba4228db
John Duncan <john@johnduncanphoto.com>
2018-01-07 13:07:37 +0000

    stable

--- client/src/app.js
+++ client/src/app.js
@@ -4,9 +4,23 @@ const Request = require('./services/request.js');
 const app = function() {
 
   const homeButton = document.querySelector('#home');
-  homeButton.addEventListener('click', function() {
-    console.log('home button clicked');
-  })
+  homeButton.addEventListener('click', homeButtonClicked);
+
+  const listViewButton = document.querySelector('#list-view');
+  homeButton.addEventListener('click', homeButtonClicked);
+
+  const aboutButton = document.querySelector('#home');
+  homeButton.addEventListener('click', homeButtonClicked);
+
+  const exploreButton = document.querySelector('#explore');
+  homeButton.addEventListener('click', homeButtonClicked);
+
+  const homeButtonClicked = function () {
+    // here requests will need to get another page.
+    console.log('home clicked');
+  }
+
+
 
   // <button type="button" name="home"></button>
   // <button type="button" name="list-view"></button>
