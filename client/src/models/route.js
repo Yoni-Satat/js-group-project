8ce328e0ebf3642e67326e6838dd7e691dd624a4
Callum <callumpb@hotmail.com>
2018-01-09 11:13:19 +0000

    created route.js + should have ID test passed

---
+++ client/src/models/route.js
@@ -0,0 +1,8 @@
+const Route = function(id, title, coordsStart, coordsEnd) {
+  this.id = id;
+  this.title = title;
+  this.coordsStart = coordsStart;
+  this.coordsEnd = coordsEnd;
+};
+
+module.exports = Route;
---
+++ specs/route_test.js
@@ -0,0 +1,16 @@
+const Route = require('../client/src/models/route.js');
+const assert = require('assert');
+
+describe('Route', function() {
+  let route1;
+
+  beforeEach(function() {
+    route1 = new Route('this is an ID', 'school', {lat: 10, lng: 10}, {lat: 20, lng: 20} );
+
+  });
+
+  it('should have an ID', function() {
+    assert.strictEqual(route1.id, 'this is an ID');
+  });
+
+});
