62e97b225ff8fd6ccdad99f7275b2f0a04d576f7
Yoni Satat <yoni.satat@gmail.com>
2018-01-10 14:10:14 +0000

    added toggleDone - test passed

--- client/src/models/route.js
+++ client/src/models/route.js
@@ -1,7 +1,12 @@
-const Route = function(title, start, end) {
+const Route = function(title, start, end, done) {
   this.title = title;
   this.start = start;
   this.end = end;
+  this.done = false;
+};
+
+Route.prototype.toggleDone = function() {
+  this.done = !this.done;
 };
 
 module.exports = Route;
