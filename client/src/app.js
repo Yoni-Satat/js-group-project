fd3b9e61d4bf6de6871ed041b10d6024a7d8c6f9
JayloneZ <andrezyczkowski@gmail.com>
2018-01-11 11:17:11 +0000

    made aboutfunction flex

--- client/src/app.js
+++ client/src/app.js
@@ -241,7 +241,7 @@ const aboutFunction = function () {
 		homeForm.innerHTML = "";
 	}
 
-	container.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	homeForm.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 }
 
 document.addEventListener('DOMContentLoaded', app);
